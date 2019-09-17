00cf48cf6fee1a19d956eafcde1b698909c01d74
TheCsWorld <43708569+TheCsWorld@users.noreply.github.com>
2019-09-17 15:48:47 +0100

    Add files via upload
    Completed test 1.

--- LowestCommonAncestor/src/LCATest.java
+++ LowestCommonAncestor/src/LCATest.java
@@ -5,8 +5,35 @@ import org.junit.Test;
 public class LCATest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		LCA tree = new LCA(); 
+		tree.root = new Node(1); 
+		tree.root.left = new Node(2); 
+		tree.root.right = new Node(3); 
+		tree.root.left.left = new Node(4); 
+		tree.root.left.right = new Node(5); 
+		tree.root.right.left = new Node(6); 
+		tree.root.right.right = new Node(7); 
+
+		System.out.println("LCA(4, 5): " + tree.findLCA(4,5)); 
+		System.out.println("LCA(4, 6): " + tree.findLCA(4,6)); 
+		System.out.println("LCA(3, 4): " + tree.findLCA(3,4)); 
+		System.out.println("LCA(2, 4): " + tree.findLCA(2,4)); 
+	}
+	
+	@Test
+	public void test2() {
+		LCA tree = new LCA(); 
+		tree.root = new Node(1); 
+		tree.root.left = new Node(2); 
+		tree.root.left.left = new Node(3);  
+		tree.root.left.left.left = new Node(4);
+		tree.root.left.left.left.left= new Node(5);
+
+//		System.out.println("LCA(4, 5): " + tree.findLCA(4,5)); 
+//		System.out.println("LCA(4, 6): " + tree.findLCA(4,6)); 
+//		System.out.println("LCA(3, 4): " + tree.findLCA(3,4)); 
+//		System.out.println("LCA(2, 4): " + tree.findLCA(2,4)); 
 	}
 
 }
