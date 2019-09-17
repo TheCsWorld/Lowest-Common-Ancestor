cdcbfc63a4209f9913833670048366806215249e
TheCsWorld <43708569+TheCsWorld@users.noreply.github.com>
2019-09-17 15:56:52 +0100

    Add files via upload

--- LowestCommonAncestor/src/LCATest.java
+++ LowestCommonAncestor/src/LCATest.java
@@ -3,6 +3,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LCATest {
+	
+	@Test
+	public void test0() {
+		LCA tree = new LCA(); 
+		
+		System.out.println("Test 0 LCA(4, 5): " + tree.findLCA(4,5)); 
+		System.out.println("Test 0 LCA(4, 6): " + tree.findLCA(4,6)); 
+		System.out.println("Test 0 LCA(8, 4): " + tree.findLCA(8,4)); 
+	}
 
 	@Test
 	public void test1() {
@@ -15,10 +24,9 @@ public class LCATest {
 		tree.root.right.left = new Node(6); 
 		tree.root.right.right = new Node(7); 
 
-		System.out.println("LCA(4, 5): " + tree.findLCA(4,5)); 
-		System.out.println("LCA(4, 6): " + tree.findLCA(4,6)); 
-		System.out.println("LCA(3, 4): " + tree.findLCA(3,4)); 
-		System.out.println("LCA(2, 4): " + tree.findLCA(2,4)); 
+		System.out.println("Test 1 LCA(4, 5): " + tree.findLCA(4,5)); 
+		System.out.println("Test 1 LCA(4, 9): " + tree.findLCA(4,9));  
+		System.out.println("Test 1 LCA(8, 7): " + tree.findLCA(8,7)); 
 	}
 	
 	@Test
@@ -28,12 +36,25 @@ public class LCATest {
 		tree.root.left = new Node(2); 
 		tree.root.left.left = new Node(3);  
 		tree.root.left.left.left = new Node(4);
-		tree.root.left.left.left.left= new Node(5);
+		tree.root.left.left.left.left= new Node(5); 
 
-//		System.out.println("LCA(4, 5): " + tree.findLCA(4,5)); 
-//		System.out.println("LCA(4, 6): " + tree.findLCA(4,6)); 
-//		System.out.println("LCA(3, 4): " + tree.findLCA(3,4)); 
-//		System.out.println("LCA(2, 4): " + tree.findLCA(2,4)); 
+		System.out.println("Test 2 LCA(4, 5): " + tree.findLCA(4,5)); 
+		System.out.println("Test 2 LCA(4, 6): " + tree.findLCA(4,6)); 
+		System.out.println("Test 2 LCA(8, 4): " + tree.findLCA(8,4)); 
+	}
+	
+	@Test
+	public void test3() {
+		LCA tree = new LCA(); 
+		tree.root = new Node(1); 
+		tree.root.right = new Node(2); 
+		tree.root.right.right = new Node(3);  
+		tree.root.right.right.right = new Node(4);
+		tree.root.right.right.right.right= new Node(5); 
+
+		System.out.println("Test 3 LCA(4, 5): " + tree.findLCA(4,5)); 
+		System.out.println("Test 3 LCA(4, 6): " + tree.findLCA(4,6)); 
+		System.out.println("Test 3 LCA(8, 4): " + tree.findLCA(8,4)); 
 	}
 
 }
