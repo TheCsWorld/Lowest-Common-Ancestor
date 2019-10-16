89063985afc81a69a61742c5df662d6cae01cc7d
TheCsWorld <43708569+TheCsWorld@users.noreply.github.com>
2019-10-16 15:03:04 +0100

    Add files via upload
    Added test

--- LowestCommonAncestor/src/LCATest.java
+++ LowestCommonAncestor/src/LCATest.java
@@ -23,14 +23,10 @@ public class LCATest {
 		tree.root.left.right = new Node(5); 
 		tree.root.right.left = new Node(6); 
 		tree.root.right.right = new Node(7);  
-
-		//System.out.println("Test 1a LCA(4, 5): " + tree.findLCA(4,5)); 
+ 
 		assertEquals("Test 1a LCA(4,5):", 2, tree.findLCA(4,5));
-		//System.out.println("Test 1b LCA(4, 9): " + tree.findLCA(4,9)); 
 		assertEquals("Test 1b, Missing node", -1, tree.findLCA(4,9));   
-		//System.out.println("Test 1c LCA(8, 7): " + tree.findLCA(8,7));
 		assertEquals("Test 1c, Missing node", -1, tree.findLCA(8,7)); 
-		//System.out.println("Test 1d LCA(8, 9): " + tree.findLCA(8,9)); 	
 		assertEquals("Test 1d LCA(8,9) missing Nodes", -1, tree.findLCA(8,9));
 	}
 	
@@ -43,11 +39,8 @@ public class LCATest {
 		tree.root.left.left.left = new Node(4);
 		tree.root.left.left.left.left= new Node(5); 
 
-//		System.out.println("Test 2a LCA(4, 5): " + tree.findLCA(4,5)); 
 		assertEquals("Test 2a LCA(4,5)", 4, tree.findLCA(4,5));
-//		System.out.println("Test 2b LCA(4, 6): " + tree.findLCA(4,6)); 
 		assertEquals("Test 2b missing node", -1, tree.findLCA(4, 6));
-//		System.out.println("Test 2c LCA(8, 4): " + tree.findLCA(8,4)); 
 		assertEquals("Test 2c missing node", -1, tree.findLCA(8, 4));
 		
 	}
@@ -61,11 +54,8 @@ public class LCATest {
 		tree.root.right.right.right = new Node(4);
 		tree.root.right.right.right.right= new Node(5); 
 
-//		System.out.println("Test 3a LCA(4, 5): " + tree.findLCA(4,5)); 
 		assertEquals("Test 3a LCA(4,5)", 4, tree.findLCA(4, 5));
-//		System.out.println("Test 3b LCA(4, 6): " + tree.findLCA(4,6)); 
 		assertEquals("Test 3b missing node", -1, tree.findLCA(4, 6));
-//		System.out.println("Test 3c LCA(8, 4): " + tree.findLCA(8,4));
 		assertEquals("Test 3c missing node", -1, tree.findLCA(8, 4));
 	}
 	
@@ -86,15 +76,10 @@ public class LCATest {
 		tree.root.right.right.right.right = new Node(12);
 		tree.root.left.left.left = new Node(13);
 
-//		System.out.println("Test 4a LCA(4, 5): " + tree.findLCA(4,5)); 
 		assertEquals("Test 4a LCA(4,5)", 2, tree.findLCA(4, 5));
-//		System.out.println("Test 4b LCA(4, 11): " + tree.findLCA(4,11)); 
 		assertEquals("Test 4b LCA(4, 11)", 1, tree.findLCA(4, 11));
-//		System.out.println("Test 4c LCA(12, 7): " + tree.findLCA(12,7)); 
 		assertEquals("Test 4c LCA(12,7)", 7, tree.findLCA(12, 7));
-//		System.out.println("Test 4d LCA(13, 9): " + tree.findLCA(13,9)); 
 		assertEquals("Test 4d LCA(13,9)", 1, tree.findLCA(13, 9));
-//		System.out.println("Test 4e LCA(5, 13): " + tree.findLCA(5,13)); 
 		assertEquals("Test 4e LCA(5,13)", 2, tree.findLCA(5, 13));
 	} 
 	
@@ -155,6 +140,16 @@ public class LCATest {
 		for(int i : expectedResult){
 			assertTrue("Testing 7a mutliple lca return", tree.lowestCommonAncestor(5,6).contains(i));
 		}
+	}
+	
+	@Test
+	public void test8(){
+		DirectedAcyclicGraph tree = new DirectedAcyclicGraph(0);
+		ArrayList<Integer> expectedResult = new ArrayList<Integer>();
+		
+		for(int i : expectedResult){
+			assertFalse("Testing empty tree", tree.lowestCommonAncestor(5,6).contains(i));
 		}
+	}
 	
 }
